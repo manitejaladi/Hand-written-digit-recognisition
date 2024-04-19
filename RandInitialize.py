c6c6cdf290b4dd58ce59d05b3a997cec6ce2339b
Mani1999 <ladimaniteja@gmail.com>
2024-04-19 15:27:19 -0400

    Add files via upload

---
+++ RandInitialize.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import numpy as np
+
+def initialise(a, b):
+    epsilon = 0.15
+    c = np.random.rand(a, b + 1) * (
+    # Randomly initialises values of thetas between [-epsilon, +epsilon]
+    2 * epsilon) - epsilon 
+    return c
+
+
+# In[ ]:
+
+
+
+
